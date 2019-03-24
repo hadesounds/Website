8603471b5f5dc809b5129ca577211128a89c3af0
Renan de Oliveira Pinto <37659262+hadesounds@users.noreply.github.com>
2019-03-24 20:41:11 -0300

    Add files via upload

--- JS/principal.js
+++ JS/principal.js
@@ -1,6 +1,16 @@
 function textOne(){
-    document.getElementById("text").innerHTML = "teste texto 1"
-    document.getElementById("title").innerText = "Project 1"
+    if (window.location.href.indexOf('&index.html')) {
+        document.getElementById("text").innerHTML = "teste texto 1"
+        document.getElementById("title").innerText = "Project 1"
+        console.log("Aqui" + window.location.href.indexOf('&index.html'));
+        
+    }
+    else{
+        document.getElementById("text").innerText = "teste texto estatística 1"
+        document.getElementById("title").innerText = "Topic 1"
+        console.log("aqui 1" + window.location.href.indexOf('statistics.html'));
+        
+    }
 }
 
 function textTwo(){
@@ -13,6 +23,6 @@ function textThree(){
     document.getElementById("title").innerText = "Project 3"
 }
 function textBio(){
-    document.getElementById("text").innerText = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis impedit repellat    harum id iure voluptate, obcaecati dolorum quidem sunt saepe recusandae reiciendis optio    cumque quisquam modi, voluptas rem hic. Eum! Lorem ipsum dolor sit amet consectetur    adipisicing elit. Nisi voluptate exercitationem soluta quod dignissimos consecteturexcepturi sequi aspernatur consequuntur. Inventore, numquam magnam. Ut atque eligendi    provident ratione, incidunt est harum. Lorem ipsum dolor sit amet consectetur adipisicing    elit. Tenetur voluptates non ratione sunt, quasi unde deserunt soluta ex ea impedit    doloribus facilis. Beatae soluta molestiae provident tenetur delectus est dolore! Loremipsum dolor sit amet consectetur adipisicing elit. Itaque illo ut delectus molestiae, architecto, iusto error autem in quasi veniam, asperiores iure ullam doloribus fuga? Esse perspiciatis sunt nihil neque. Lorem    ipsum dolor sit amet consectetur adipisicing elit. Voluptatem exercitationem, accusamus    vitae ut numquam, dolore nam qui itaque corrupti molestiae magnam mollitia iste aliquidmodi. Ullam quam praesentium pariatur in! Lorem ipsum dolor sit amet consectetur    adipisicing elit. Est, adipisci quod. Error cupiditate quos voluptatem doloremque vel quae    dolorum, alias quod culpa expedita, placeat necessitatibus minus obcaecati autem,   reprehenderit voluptates. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum     magni asperiores debitis ipsum doloremque sequi a pariatur. Fuga, fugiat molestiae quos    similique repellat cupiditate aut laborum consectetur placeat, illum hic. Lorem, ipsum    dolor sit amet consectetur adipisicing elit. Eligendi dolor, blanditiis earum accusamus id    ipsa adipisci, provident culpa molestias modi ipsum ut, quidem aliquam reprehenderit omnis    distinctio. Beatae, quae amet. Lorem ipsum dolor sit amet, consectetur adipisicing elit.    Sequi, nulla voluptatum? Sed velit nobis ullam quos eligendi, dolorem obcaecati    repellendus, dignissimos sint, magni iusto eaque doloremque voluptatem dolor omnis minima."
+    document.getElementById("text").innerText = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis impedit repellat    harum id iure voluptate, obcaecati dolorum quidem sunt saepe recusandae reiciendis optio    cumque quisquam modi, voluptas rem hic. Eum! Lorem ipsum dolor sit amet consectetur    adipisicing elit. Nisi voluptate exercitationem soluta quod dignissimos consecteturexcepturi sequi aspernatur consequuntur. Inventore, numquam magnam. Ut atque eligendi    provident ratione, incidunt est harum. Lorem ipsum dolor sit amet consectetur adipisicing    elit. Tenetur voluptates non ratione sunt, quasi unde deserunt soluta ex ea impedit    doloribus facilis. Beatae soluta molestiae provident tenetur delectus est dolore! Loremipsum dolor sit amet consectetur adipisicing elit. Itaque illo ut dele"
     document.getElementById("title").innerText = "Bio"
 }
